5826cb9f3966efb123a941bbd351cd8c408cc448
CodingMania <vasilmitovv@gmail.com>
2024-06-04 18:35:19 +0300

    Add buttons and styles for ring size list component

--- src/components/RingSizeList/RingSizeList.jsx
+++ src/components/RingSizeList/RingSizeList.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { Nav, ListGroup, ListGroupItem } from "react-bootstrap"
+import { Nav, ListGroup, ListGroupItem, Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 import * as ringSizesService from '../../services/ringSizesService'
@@ -15,10 +15,19 @@ export default function RingSizeList() {
     }, [])
 
     return (
-            <div className="ring-size-wrapper">
+        <div className="ring-size-wrapper">
             <h2>Списък с рамери</h2>
             <Nav.Link as={Link} to="/administration/ring-size/create"><strong>Добави нов</strong></Nav.Link>
-            {ringSizes.length > 0 ? <ListGroup>{ringSizes.map(r =><ListGroupItem key={r._id}>{r.size}</ListGroupItem> )}</ListGroup> : <h4>Все още няма размери</h4>}
-        </div> 
+            {ringSizes.length > 0 ?
+                <ListGroup>
+                    {ringSizes.map(r =>
+                        <ListGroupItem key={r._id}>
+                            {r.size}
+                            <Button className="ring-size-edit-btn" variant="info"><i class="bi bi-pencil-fill"></i></Button>
+                            <Button className="ring-size-delete-btn" variant="danger"><i class="bi bi-x-circle"></i></Button>
+                        </ListGroupItem>)}
+                </ListGroup>
+                : <h4>Все още няма размери</h4>}
+        </div>
     )
 }
