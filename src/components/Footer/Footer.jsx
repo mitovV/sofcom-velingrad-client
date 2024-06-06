0b45a607123e2577694629357f028a5e98dd5f91
CodingMania <vasilmitovv@gmail.com>
2024-06-06 17:40:47 +0300

    Add styles for header, footer and side bar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,21 @@
 import './Footer.css'
+import { Container, Row, Col } from 'react-bootstrap'
 
-export default function Footer(){
+export default function Footer() {
     return (
         <footer>
-            <div className='container'>
-            <i className="bi bi-envelope"> sofcomvelingrad@gmail.com</i>
-            <i className='bi bi-phone'>0897099098</i>
-            </div>
-            </footer>
+            <Container className='container'>
+                <Row>
+                    <Col className='contacts'>
+                        <div>Контакти</div>
+                        <div><i className="bi bi-envelope"> sofcomvelingrad@gmail.com</i></div>
+                        <div><i className='bi bi-phone'>0897099098</i></div>
+                    </Col>
+                    <Col className="date-wrapper">
+                        &copy; {new Date().getFullYear()} - Софком Велинград
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
