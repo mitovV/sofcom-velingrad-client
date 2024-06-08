7ad2ff322b12c69d36398cceaf43521dedd3a583
CodingMania <vasilmitovv@gmail.com>
2024-06-08 22:23:51 +0300

    Add categories list

--- src/components/Administration/CategoriesList/CategoriesList.jsx
+++ src/components/Administration/CategoriesList/CategoriesList.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react"
+import { Nav } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 import BaseListing from "../../Shared/BaseListing/BaseListing"
 
 import * as categoriesService from '../../../services/categoriesService'
 
+import './CategoriesList.css'
+
 export default function CategoriesList() {
     const [categories, setCategorise] = useState([])
+    const modalMessage = 'Сигурни ли сте, че искате да изтриете тази категория?'
+    const [rerender, setRerender] = useState(false)
+
 
     useEffect(() => {
         categoriesService.getAll()
@@ -13,9 +20,29 @@ export default function CategoriesList() {
             .catch(err => console.error(err))
     }, [])
 
+    const onDeleteHandler = (_id) => {
+
+        categories.deleteBySize(_id)
+            .then()
+            .catch(err => console.error(err))
+    }
+
+    //TODO: Create logic for delete and edit
     return (
-        categories.length > 0 ? 
-        <BaseListing data={categories} value='name' path='' classes=''></BaseListing> 
-        : <h3>LoADING...</h3>
+        <div className="categories-wrapper">
+            <h2>Списък с категории</h2>
+            <Nav.Link as={Link} className="add-new-category" to="/administration/categories/create"><strong>Добави нова категория</strong></Nav.Link>
+            {categories.length > 0 ?
+                <BaseListing
+                    data={categories}
+                    name='name'
+                    path='/administration/categories/edit/'
+                    message={modalMessage}
+                    setRerender={setRerender}
+                    onDeleteHandler={onDeleteHandler}
+                >
+                </BaseListing>
+                : <h4>Все още няма категории</h4>}
+        </div>
     )
 }
