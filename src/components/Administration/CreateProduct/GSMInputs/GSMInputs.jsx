f3221d92f536b37bdb00abbdb9dbea45049d797c
CodingMania <vasilmitovv@gmail.com>
2024-08-22 16:45:46 +0300

    Extend GSMInputs component

--- src/components/Administration/CreateProduct/GSMInputs/GSMInputs.jsx
+++ src/components/Administration/CreateProduct/GSMInputs/GSMInputs.jsx
@@ -3,8 +3,16 @@ import { Form } from "react-bootstrap"
 export default function GSMInputs() {
     return (
         <Form.Group className='mb-3'>
-            <Form.Label>Име</Form.Label>
-            <Form.Control name='title'></Form.Control>
+            <Form.Label>Модел</Form.Label>
+            <Form.Control type="text" name='model'></Form.Control>
+            <Form.Label>RAM памет</Form.Label>
+            <Form.Control type="number" name='ram'></Form.Control>
+            <Form.Label>Вътрешна памет</Form.Label>
+            <Form.Control type="number" name='rom'></Form.Control>
+            <Form.Label>Описание</Form.Label>
+            <Form.Control name="description" as="textarea" rows={3}/>
+            <Form.Label>Цена</Form.Label>
+            <Form.Control placeholder="Цена" name="price" type="number" min={0} step={0.1}/>
         </Form.Group>
     )
 }
--- src/components/Administration/CreateProduct/TechniqueInputs/TechniqueInputs.jsx
+++ src/components/Administration/CreateProduct/TechniqueInputs/TechniqueInputs.jsx
@@ -3,14 +3,14 @@ import { Form, FormControl, FormGroup, FormLabel, FormText } from "react-bootstr
 export default function TechniqueInputs(){
     return (
         <Form>
-            <FormGroup>
-                <FormLabel>Вещ</FormLabel>
-                <FormControl type="text" name="title"/>
-                <FormLabel>Описание</FormLabel>
-                <FormControl name="description" as="textarea" rows={3}/>
-                <FormLabel>Цена</FormLabel>
-                <FormControl name="price" type="number" min={0} step={0.1}/>
-            </FormGroup>
+            <Form.Group>
+                <Form.Label>Вещ</Form.Label>
+                <Form.Control type="text" name="title"/>
+                <Form.Label>Описание</Form.Label>
+                <Form.Control name="description" as="textarea" rows={3}/>
+                <Form.Label>Цена</Form.Label>
+                <Form.Control name="price" type="number" min={0} step={0.1}/>
+            </Form.Group>
         </Form>
     )
 }
