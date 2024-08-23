d211fbb842bf17dab1ebeb6cb00536d390a8f490
CodingMania <vasilmitovv@gmail.com>
2024-08-23 15:35:50 +0300

    Add silver carat service

---
+++ src/services/silverCaratsService.js
@@ -0,0 +1,16 @@
+import config from "../config/config"
+
+const categoryUrl = config.BASE_SERVER_URL + 'silver-carats/'
+
+export const all = async () => {
+    let url = categoryUrl + 'all'
+
+    try {
+        const response = await fetch(url)
+        let data = await response.json()
+        return data
+
+    } catch (message) {
+        return console.error(message)
+    }
+}
